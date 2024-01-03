508680ecb41aa378c20954ad7513c04d1d6128c0
Rafisto <1.rafal.wlodarczyk@gmail.com>
2024-01-03 23:03:29 +0100

    CORS `backend`

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,8 @@ pub mod tasks;
 use tasks::{get_todo_list, add_element_to_list, remove_element_from_list, update_task};
 use rocket::serde::json::{json, Json, Value};
 use backend::models::{NewTask, UpdateTask};
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 
 #[macro_use]
 extern crate rocket;
@@ -45,5 +47,20 @@ fn update_todo(task_id: i32, updated_task: Json<UpdateTask>) -> Value {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, get_todo, add_todo, delete_todo, update_todo])
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::all(),
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete, Method::Patch]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+        .to_cors()
+        .expect("Failed to create CORS configuration.");
+
+    rocket::build()
+        .attach(cors)
+        .mount("/", routes![hello, get_todo, add_todo, delete_todo, update_todo])
 }
